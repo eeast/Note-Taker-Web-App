67ab2fadfc1d52a2e54764cb8d5bda540fcda2bc
eeast <eeasthope@gmail.com>
2023-04-22 21:52:07 -0500

    updated port variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index');
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
